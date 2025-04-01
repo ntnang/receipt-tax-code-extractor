923913372f99322e897b163148130a068a584b09
ntnang <ntnang91@gmail.com>
2025-04-01 17:04:28 +0700

    replace placeholders with preserved formatted text and number to text conversion

--- receipt_creator.py
+++ receipt_creator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from docx import Document
-# import shutil as sh
+from vietnam_number import n2w
 
 def generate_receipts_from_template_with_filled_data():
     data_file = "data.xlsx"
@@ -16,8 +16,6 @@ def generate_receipts_from_template_with_filled_data():
         row_dict = {i: value for i, value in enumerate(df.iloc[index])}
         replace_placeholders(template_file, f"filled_document_{index}.docx", row_dict)
 
-    # return dict(zip(df['Placeholder'], df['Value']))  # Convert to dictionary
-
 def replace_placeholders(doc_file, output_file, replacements):
     # sh.copy(doc_path, output_path)
 
@@ -26,30 +24,31 @@ def replace_placeholders(doc_file, output_file, replacements):
     
     # Replace placeholders in paragraphs
     for paragraph in doc.paragraphs:
-        for key, value in replacements.items():
-            placeholder = f"{{{key}}}"
-            if placeholder in paragraph.text:
-                paragraph.text = paragraph.text.replace(placeholder, str(value))
+        for run in paragraph.runs:
+            for key, value in replacements.items():
+                placeholder = f"{{{key}}}"
+                if placeholder in run.text:
+                    paragraph.text = run.text.replace(placeholder, str(value))
     
     # Replace placeholders in tables
     for table in doc.tables:
         for row in table.rows:
             for cell in row.cells:
-                for key, value in replacements.items():
-                    placeholder = f"{{{key}}}"
-                    if placeholder in cell.text:
-                        cell.text = cell.text.replace(placeholder, str(value))
+                for paragraph in cell.paragraphs:
+                    for run in paragraph.runs:
+                        for key, value in replacements.items():
+                            placeholder = f"{{{key}}}"
+                            if placeholder in run.text:
+                                run.text = run.text.replace(placeholder, str(value))
+                            if "{#}" in run.text:
+                                run.text = run.text.replace("{#}", n2w(str(replacements.get(5))))
 
     doc.save(output_file)
     print(f"File saved as {output_file}")
 
-# File paths
-# excel_file = "data.xlsx"
-# word_template = "template.docx"
-# output_word = "filled_document.docx"
-
-# Process the files
-# replacements = read_replacements_from_excel(excel_file)
-# replace_placeholders(word_template, output_word, replacements)
+# def num_to_text(num):
+#     text = ""
+#     digit_text = ["không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"]
+#     a = ["mười", "mươi", "trăm", "ngàn", "triệu", "tỷ"]
 
 generate_receipts_from_template_with_filled_data()
